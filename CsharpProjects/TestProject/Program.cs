e6d2bbfa093b9f59388f034aab8724a239ed639b
Jonathan Anleu <jonathan.anleu@gmail.com>
2025-01-03 13:13:48 -0600

    solving data conversion challenges

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -1,22 +1,5 @@
-﻿string? readResult;
-bool validEntry = false;
-Console.Write("Enter your role name (Administrator, Manager, or User): ");
-do
-{
-    readResult = Console.ReadLine().ToLower().Trim();
-    if (readResult != null)
-    {
-        if (readResult == "administrator" || readResult == "manager" || readResult == "user")
-        {
-           validEntry = true;
-           Console.WriteLine($"Your input value \"{readResult}\" has been accepted.");
-        }
-        else
-        {
-            Console.WriteLine($"The role name that you entered, \"{readResult}\" is not valid. Enter your role");
-        }
-    }
-} while (validEntry == false);
+﻿string [] values = ["12.3", "45", "ABC", "11", "DEF"];
+
 
 
 
--- CsharpProjects/TestProject/obj/Debug/net9.0/TestProject.AssemblyInfo.cs
+++ CsharpProjects/TestProject/obj/Debug/net9.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6e78711d46dc2cfe2708ff01fc61515ddcedd31")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+098dc5e675a6c9c41bbd47f768eca89f394adc50")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
