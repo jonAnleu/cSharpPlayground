960a2af0eef0c7f435fae7154581ce4743a34bb6
Jonathan Anleu <jonathan.anleu@gmail.com>
2025-01-03 13:54:40 -0600

    try parse challenge

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -1,5 +1,21 @@
-﻿string [] values = ["12.3", "45", "ABC", "11", "DEF"];
+﻿string[] values = ["12.3", "45", "ABC", "11", "DEF"];
 
+decimal total = 0m;
+string message = "";
 
+for (int i = 0; i < values.Length; i++)
+{
+    decimal number;
+    if (decimal.TryParse(values[i], out number))
+    {
+        total += number;
+    } else
+    {
+        message += values[i];
+    }
+ 
+}
 
+Console.WriteLine($"Message: {message}");
+Console.WriteLine($"Total: {total}");
 
